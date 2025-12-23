497dfac80fd3eb95bcba8857a6a3c5d31b24d1e3
spongebob888 <121923937+spongebob888@users.noreply.github.com>
2025-12-23 22:09:20 +0800

    chore: adding nix flake support

---
+++ flake.nix
@@ -0,0 +1,67 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    crane.url = "github:ipetkov/crane";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, crane }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        libraries = with pkgs;[
+          glib
+          openssl_3.dev
+          sqlite
+          libclang
+        ];
+        pythonEnv = pkgs.python3.withPackages (ps: with ps; [
+	          pycurl
+            pysocks
+            dnspython
+          ]);
+
+        craneLib = crane.mkLib pkgs;
+        packages = with pkgs; [
+          curl
+          pythonEnv
+          wget
+          sqlite
+          pkg-config
+          openssl_3
+          glib
+          cmake
+          protobuf
+          protoc-gen-rust
+		      clang
+          samply
+          iperf3
+          sing-box
+          killall
+          act
+          rustup
+          uv
+        ];
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = packages;
+
+          shellHook =
+            ''
+              export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
+              export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
+            '';
+        };
+      packages.default = craneLib.buildPackage rec {
+        src = craneLib.cleanCargoSource ./.;
+        pname = "clash-rs";
+        doCheck = false;
+        # Add extra inputs here or any other derivation settings
+        # doCheck = true;
+        # buildInputs = [];
+        # nativeBuildInputs = [];
+      };
+      });
+}
